37cf69bf8e1443ee6ff137688c800ab7b34ecd29
parmartarun <parmartarun.dev@gmail.com>
2024-01-24 09:28:22 -0500

    ⚡️ Add Home Page for restaurant app

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# from .models import Menu
 
 # Create your views here.
+
+
+def index(req):
+    return render(req, 'index.html')
+
+
+# def menu(req):
+#     menu_data = Menu.objects.all()
+#     main_data = {'menu': menu_data}
+#     return render(req, 'menu.html', {'menu': main_data})
